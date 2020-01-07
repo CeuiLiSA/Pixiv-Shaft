1ad3fd18b7237f323633ea08a4327c082d988062
CeuiLiSA <Mercis09bv>
2020-01-07 11:22:06 +0800

    删除gif

--- gif/src/main/java/ceui/lisa/gif/core/GifState.java
+++
@@ -1,9 +0,0 @@
-package ceui.lisa.gif.core;
-
-public class GifState {
-
-    public static final int ST1 = 1; // 未下载zip包
-    public static final int ST2 = 2; // 下载了zip包未解压
-    public static final int ST3 = 3; // 解压了zip包
-    public static final int ST4 = 4; // 已生成了GIF文件
-}
