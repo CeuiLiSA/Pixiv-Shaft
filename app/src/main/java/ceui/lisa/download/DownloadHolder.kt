c991b2dbee8d178271c19e77e2144101e41d46ae
Aragaki <4ragaki@gmail.com>
2020-05-11 16:11:12 +0800

    DownloadHolder fix

---
+++ app/src/main/java/ceui/lisa/download/DownloadHolder.kt
@@ -0,0 +1,20 @@
+package ceui.lisa.download
+
+import ceui.lisa.adapters.ViewHolder
+import ceui.lisa.databinding.RecyDownloadTaskBinding
+
+class DownloadHolder(binding: RecyDownloadTaskBinding) : ViewHolder<RecyDownloadTaskBinding>(binding) {
+    var taskId = 0
+
+    fun onUpdate(progress: Progress) {
+        baseBind.progress.max = progress.max
+        baseBind.progress.progress = progress.progress
+        baseBind.currentSize.text = progress.text
+    }
+
+    class Progress() {
+        var progress: Int = 0
+        var max: Int = 0
+        var text: String = ""
+    }
+}
