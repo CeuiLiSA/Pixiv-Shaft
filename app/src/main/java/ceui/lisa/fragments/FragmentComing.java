f1c6b593ef369503127600f25f2f7b801e869c8a
CeuiLiSA <32817377+CeuiLiSA@users.noreply.github.com>
2020-06-23 15:07:00 +0800

    开发表情功能

--- app/src/main/java/ceui/lisa/fragments/FragmentComing.java
+++ app/src/main/java/ceui/lisa/fragments/FragmentComing.java
@@ -1,36 +1,16 @@
 package ceui.lisa.fragments;
 
-import android.os.Bundle;
-import android.text.Editable;
-import android.text.Html;
-import android.text.SpannableString;
-import android.text.Spanned;
-import android.text.TextWatcher;
-import android.text.method.LinkMovementMethod;
-import android.view.KeyEvent;
 import android.view.View;
-import android.widget.LinearLayout;
 
 import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.effective.android.panel.PanelSwitchHelper;
-import com.effective.android.panel.interfaces.listener.OnKeyboardStateListener;
-import com.effective.android.panel.interfaces.listener.OnPanelChangeListener;
-import com.effective.android.panel.view.panel.IPanelView;
-import com.effective.android.panel.view.panel.PanelView;
-
-import org.jetbrains.annotations.Nullable;
-
-import java.util.ArrayList;
-import java.util.List;
 
 import ceui.lisa.R;
 import ceui.lisa.adapters.EmojiAdapter;
-import ceui.lisa.core.ImgGetter;
 import ceui.lisa.databinding.FragmentComingBinding;
 import ceui.lisa.interfaces.OnItemClickListener;
-import ceui.lisa.model.Content;
 import ceui.lisa.model.EmojiItem;
 import ceui.lisa.utils.Common;
 import ceui.lisa.utils.Emoji;
@@ -51,7 +31,7 @@ public class FragmentComing extends BaseFragment<FragmentComingBinding> {
             mHelper = new PanelSwitchHelper.Builder(this)
                     .contentCanScrollOutside(true)    //可选模式，默认true，当面板实现时内容区域是否往上滑动
                     .logTrack(true)                   //可选，默认false，是否开启log信息输出
-                    .build(false);	          //可选，默认false，是否默认打开输入法
+                    .build(false);              //可选，默认false，是否默认打开输入法
         }
     }
 
@@ -65,58 +45,32 @@ public class FragmentComing extends BaseFragment<FragmentComingBinding> {
         adapter.setOnItemClickListener(new OnItemClickListener() {
             @Override
             public void onItemClick(View v, int position, int viewType) {
-
                 final EmojiItem item = adapter.getItemAt(position);
-                if (selection < current.size()) {
-                    current.add(selection, item);
+
+                String name = item.getName();
+                String show = baseBind.inputBox.getText().toString();
+                if (selection < show.length()) {
+                    String left = show.substring(0, selection);
+                    String right = show.substring(selection);
+                    commitString = left + name + right;
 
 
-                    Content temp = new Content();
-                    temp.setEmoji(true);
-                    content.add(selection, temp);
+
+                    baseBind.inputBox.setText(left + name + right);
+                    baseBind.inputBox.setSelection(selection + name.length());
                 } else {
-                    current.add(item);
+                    String result = show + name;
+                    commitString = result;
 
 
-                    Content temp = new Content();
-                    temp.setEmoji(true);
-                    content.add(temp);
+                    baseBind.inputBox.setText(result);
+                    baseBind.inputBox.setSelection(result.length());
                 }
-
-
-                SpannableString spannableString = new SpannableString(Html.fromHtml(Emoji.transform(emojiToString()),
-                        new ImgGetter(baseBind.inputBox), null));
-                baseBind.inputBox.setText(spannableString);
-
-
-                if (selection < current.size()) {
-                    baseBind.inputBox.setSelection(selection + 1);
-                } else {
-                    current.add(adapter.getItemAt(position));
-                    baseBind.inputBox.setSelection(baseBind.inputBox.getText().toString().length());
-                    selection = baseBind.inputBox.getText().toString().length();
-                }
-
-
+                Common.showLog(className + selection);
 
             }
         });
         recyclerView.setAdapter(adapter);
-        baseBind.inputBox.setOnKeyListener(new View.OnKeyListener() {
-            @Override
-            public boolean onKey(View v, int keyCode, KeyEvent event) {
-                if(keyCode == KeyEvent.KEYCODE_DEL && event.getAction() != KeyEvent.ACTION_UP) {
-                    if (selection > 0) {
-                        final int index = selection - 1;
-                        if (index < current.size()) {
-                            current.remove(index);
-                            selection = selection - 1;
-                        }
-                    }
-                }
-                return false;
-            }
-        });
         baseBind.inputBox.setOnSelectionChange(new EditTextWithSelection.OnSelectionChange() {
             @Override
             public void onChange(int start, int end) {
@@ -125,36 +79,11 @@ public class FragmentComing extends BaseFragment<FragmentComingBinding> {
                 }
             }
         });
-        baseBind.inputBox.addTextChangedListener(new TextWatcher() {
-            @Override
-            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
-
-            }
-
-            @Override
-            public void onTextChanged(CharSequence s, int start, int before, int count) {
-                Common.showLog(className + "afterTextChanged " + s + " " + "￼".equals(s.toString()));
-
-            }
-
-            @Override
-            public void afterTextChanged(Editable s) {
-            }
-        });
     }
 
     private int selection = 0;
 
-    private List<EmojiItem> current = new ArrayList<>();
-    private List<Content> content = new ArrayList<>();
-
-    private String emojiToString() {
-        String result = "";
-        for (EmojiItem emojiItem : current) {
-            result = result + emojiItem.getName();
-        }
-        return result;
-    }
+    private String commitString = "";
 
     @Override
     public void initLayout() {
