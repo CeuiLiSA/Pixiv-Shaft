ff172dbe8c0f9ad20003045c04a3acf99769cd6e
CeuiLiSA <32817377+CeuiLiSA@users.noreply.github.com>
2020-11-26 11:17:40 +0800

    GlideUrl重构

---
+++ app/src/main/java/ceui/lisa/utils/GlideUrlChild.java
@@ -0,0 +1,41 @@
+package ceui.lisa.utils;
+
+import android.net.Uri;
+
+import com.bumptech.glide.load.model.GlideUrl;
+import com.bumptech.glide.load.model.Headers;
+
+import java.net.URL;
+import java.util.HashMap;
+
+public class GlideUrlChild extends GlideUrl {
+
+    public GlideUrlChild(URL url) {
+        super(url);
+    }
+
+    public GlideUrlChild(String url) {
+        this(url, getHeaders(url));
+    }
+
+    public GlideUrlChild(URL url, Headers headers) {
+        super(url, headers);
+    }
+
+    public GlideUrlChild(String url, Headers headers) {
+        super(url, headers);
+    }
+
+    private static Headers getHeaders(String url) {
+        HashMap<String, String> hashMap = new HashMap<>();
+        hashMap.put(Params.MAP_KEY, Params.IMAGE_REFERER);
+        hashMap.put(Params.USER_AGENT, Params.PHONE_MODEL);
+        try {
+            hashMap.put(Params.HOST, Uri.parse(url).getHost());
+        } catch (Exception e) {
+            e.printStackTrace();
+            hashMap.put(Params.HOST, Params.HOST_NAME);
+        }
+        return () -> hashMap;
+    }
+}
--- app/src/main/java/ceui/lisa/utils/GlideUtil.java
+++ app/src/main/java/ceui/lisa/utils/GlideUtil.java
@@ -1,57 +1,46 @@
 package ceui.lisa.utils;
 
+import android.net.Uri;
 import android.text.TextUtils;
 
 import com.bumptech.glide.load.model.GlideUrl;
 import com.bumptech.glide.load.model.Headers;
 
 import java.util.HashMap;
+import java.util.Map;
 
 import ceui.lisa.models.UserBean;
 import ceui.lisa.models.IllustsBean;
 
 public class GlideUtil {
 
-    private static final Headers sHeaders = () -> {
-        HashMap<String, String> hashMap = new HashMap<>();
-        hashMap.put(Params.MAP_KEY, Params.IMAGE_REFERER);
-        hashMap.put(Params.USER_AGENT, Params.PHONE_MODEL);
-        hashMap.put(Params.HOST, Params.HOST_NAME);
-        return hashMap;
-    };
-
     public static GlideUrl getMediumImg(IllustsBean illustsBean) {
-        Common.showLog("展示image " + illustsBean.getImage_urls().getMedium());
-        return new GlideUrl(illustsBean.getImage_urls().getMedium(), sHeaders);
+        return new GlideUrlChild(illustsBean.getImage_urls().getMedium());
     }
 
     public static GlideUrl getMediumImg(String imageUrl) {
-        return new GlideUrl(imageUrl, sHeaders);
+        return new GlideUrlChild(imageUrl);
     }
 
     public static GlideUrl getArticle(String url) {
-        return new GlideUrl(url, sHeaders);
+        return new GlideUrlChild(url);
     }
 
     public static GlideUrl getLargeImage(IllustsBean illustsBean) {
-        return new GlideUrl(illustsBean.getImage_urls().getLarge(), sHeaders);
+        return new GlideUrlChild(illustsBean.getImage_urls().getLarge());
     }
 
     public static GlideUrl getHead(UserBean userBean) {
         if (userBean == null) {
-            Common.showLog("getHead userBean == null");
             return null;
         }
 
         if (userBean.getProfile_image_urls() == null) {
-            Common.showLog("getHead userBean.getProfile_image_urls == null");
             return null;
         }
 
         if (!TextUtils.isEmpty(userBean.getProfile_image_urls().getMaxImage())) {
-            Common.showLog("getHead userBean.getProfile_image_urls.getMaxImage " +
-                    userBean.getProfile_image_urls().getMaxImage());
-            return new GlideUrl(userBean.getProfile_image_urls().getMaxImage(), sHeaders);
+            return new GlideUrlChild(userBean.getProfile_image_urls().getMaxImage());
         }
 
         return null;
@@ -59,25 +48,23 @@ public class GlideUtil {
 
 
     public static GlideUrl getSquare(IllustsBean illustsBean) {
-        return new GlideUrl(illustsBean.getImage_urls().getSquare_medium(), sHeaders);
+        return new GlideUrlChild(illustsBean.getImage_urls().getSquare_medium());
     }
 
     public static GlideUrl getLargeImage(IllustsBean illustsBean, int i) {
-        Common.showLog("GlideUrl getLargeImage");
         if (illustsBean.getPage_count() == 1) {
             return getLargeImage(illustsBean);
         } else {
-            return new GlideUrl(illustsBean.getMeta_pages().get(i).getImage_urls().getLarge(), sHeaders);
+            return new GlideUrlChild(illustsBean.getMeta_pages().get(i).getImage_urls().getLarge());
         }
     }
 
 
     public static GlideUrl getOriginal(IllustsBean illustsBean, int i) {
-        Common.showLog("GlideUrl getOriginal");
         if (illustsBean.getPage_count() == 1) {
-            return new GlideUrl(illustsBean.getMeta_single_page().getOriginal_image_url(), sHeaders);
+            return new GlideUrlChild(illustsBean.getMeta_single_page().getOriginal_image_url());
         } else {
-            return new GlideUrl(illustsBean.getMeta_pages().get(i).getImage_urls().getOriginal(), sHeaders);
+            return new GlideUrlChild(illustsBean.getMeta_pages().get(i).getImage_urls().getOriginal());
         }
     }
 }
