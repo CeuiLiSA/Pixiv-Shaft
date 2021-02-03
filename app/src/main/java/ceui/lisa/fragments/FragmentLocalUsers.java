21ecd019862b374aaa40e3a0bc03d0c1eef01b6c
sunbeams001 <sunbeams001@gmail.com>
2021-02-03 12:14:52 +0800

    修复一些夜间样式问题

--- app/src/main/java/ceui/lisa/fragments/FragmentLocalUsers.java
+++ app/src/main/java/ceui/lisa/fragments/FragmentLocalUsers.java
@@ -88,7 +88,7 @@ public class FragmentLocalUsers extends BaseFragment<FragmentLocalUserBinding> {
                     public void success(List<UserModel> userModels) {
                         if (userModels.size() != 0) {
                             for (int i = 0; i < userModels.size(); i++) {
-                                View v = LayoutInflater.from(mContext).inflate(R.layout.recy_loal_user, null);
+                                View v = LayoutInflater.from(mContext).inflate(R.layout.recy_local_user, null);
                                 bindData(v, userModels.get(i));
                                 baseBind.userList.addView(v);
                             }
