d6384ba0d6c9dac0f62256846deed2463b564d2d
sunbeams001 <sunbeams001@gmail.com>
2021-02-04 23:00:18 +0800

    修复GIF下载时多出一张静态图的问题

--- app/src/main/java/ceui/lisa/helper/Android10DownloadFactory22.kt
+++ app/src/main/java/ceui/lisa/helper/Android10DownloadFactory22.kt
@@ -10,7 +10,6 @@ import ceui.lisa.core.DownloadItem
 import ceui.lisa.download.ImageSaver
 import ceui.lisa.file.LegacyFile
 import ceui.lisa.utils.Common
-import ceui.lisa.utils.Settings
 import com.blankj.utilcode.util.FileUtils
 import com.blankj.utilcode.util.PathUtils
 import okhttp3.Response
@@ -28,30 +27,15 @@ class Android10DownloadFactory22 constructor(
 
     override fun query(): Uri? {
         if (Common.isAndroidQ()) {
-            val relativePath: String = Environment.DIRECTORY_PICTURES + "/ShaftImages"
-            val relativePathR18: String = Environment.DIRECTORY_PICTURES + "/ShaftImages-R18"
-            if (item.illust.isR18File && Shaft.sSettings.isR18DivideSave) {
-                return MediaStore.Images.Media.EXTERNAL_CONTENT_URI.query(
-                    context,
-                    item.name,
-                    relativePathR18
-                )
-            } else {
-                return MediaStore.Images.Media.EXTERNAL_CONTENT_URI.query(
-                    context,
-                    item.name,
-                    relativePath
-                )
-            }
+            val relativePath: String = getRelativePathQ()
+            return MediaStore.Images.Media.EXTERNAL_CONTENT_URI.query(
+                context,
+                item.name,
+                relativePath
+            )
         } else {
-            if (item.illust.isR18File && Shaft.sSettings.isR18DivideSave) {
-                val file =
-                    File("${PathUtils.getExternalPicturesPath()}/ShaftImages-R18/${item.name}")
-                return Uri.fromFile(file)
-            } else {
-                val file = File("${PathUtils.getExternalPicturesPath()}/ShaftImages/${item.name}")
-                return Uri.fromFile(file)
-            }
+            val file = File(getFileName())
+            return Uri.fromFile(file)
         }
     }
 
@@ -64,77 +48,35 @@ class Android10DownloadFactory22 constructor(
         } else {
             // 大于等于 android 10， 使用 contentResolver insert 生成文件
             if (Common.isAndroidQ()) {
-                val relativePath: String = Environment.DIRECTORY_PICTURES + "/ShaftImages"
-                val relativePathR18: String = Environment.DIRECTORY_PICTURES + "/ShaftImages-R18"
-                if (item.illust.isR18File && Shaft.sSettings.isR18DivideSave) {
-                    val uri = MediaStore.Images.Media.EXTERNAL_CONTENT_URI.query(
-                        context,
-                        item.name,
-                        relativePathR18
-                    )
-                    if (uri != null) {
-                        val outputStream: OutputStream =
-                            context.contentResolver.openOutputStream(uri, "rwt")!!
-                        outputStream.write(ByteArray(0))
-                        outputStream.flush()
-                        outputStream.close()
-                        fileUri = uri
-                        return fileUri
-                    }
-                    fileUri = ContentValues().run {
-                        put(MediaStore.MediaColumns.RELATIVE_PATH, relativePathR18) // 下载到指定目录
-                        put(MediaStore.MediaColumns.DISPLAY_NAME, item.name) // 文件名
-                        // 取contentType响应头作为文件类型
-                        put(
-                            MediaStore.MediaColumns.MIME_TYPE,
-                            response.body?.contentType().toString()
-                        )
-                        context.contentResolver.insert(
-                            MediaStore.Images.Media.EXTERNAL_CONTENT_URI,
-                            this
-                        )
-                        // 当相同路径下的文件，在文件管理器中被手动删除时，就会插入失败
-                    } ?: throw NullPointerException("Uri insert failed. Try changing filename")
-                    return fileUri
-                } else {
-                    val uri = MediaStore.Images.Media.EXTERNAL_CONTENT_URI.query(
-                        context,
-                        item.name,
-                        relativePath
-                    )
-                    if (uri != null) {
-                        val outputStream: OutputStream =
-                            context.contentResolver.openOutputStream(uri, "rwt")!!
-                        outputStream.write(ByteArray(0))
-                        outputStream.flush()
-                        outputStream.close()
-                        fileUri = uri
-                        return fileUri
-                    }
-                    fileUri = ContentValues().run {
-                        put(MediaStore.MediaColumns.RELATIVE_PATH, relativePath) // 下载到指定目录
-                        put(MediaStore.MediaColumns.DISPLAY_NAME, item.name) // 文件名
-                        // 取contentType响应头作为文件类型
-                        put(
-                            MediaStore.MediaColumns.MIME_TYPE,
-                            response.body?.contentType().toString()
-                        )
-                        context.contentResolver.insert(
-                            MediaStore.Images.Media.EXTERNAL_CONTENT_URI,
-                            this
-                        )
-                        // 当相同路径下的文件，在文件管理器中被手动删除时，就会插入失败
-                    } ?: throw NullPointerException("Uri insert failed. Try changing filename")
+                val relativePath: String = getRelativePathQ()
+                val uri = query()
+                if (uri != null) {
+                    val outputStream: OutputStream =
+                        context.contentResolver.openOutputStream(uri, "rwt")!!
+                    outputStream.write(ByteArray(0))
+                    outputStream.flush()
+                    outputStream.close()
+                    fileUri = uri
                     return fileUri
                 }
+                fileUri = ContentValues().run {
+                    put(MediaStore.MediaColumns.RELATIVE_PATH, relativePath) // 下载到指定目录
+                    put(MediaStore.MediaColumns.DISPLAY_NAME, item.name) // 文件名
+                    // 取contentType响应头作为文件类型
+                    put(
+                        MediaStore.MediaColumns.MIME_TYPE,
+                        response.body?.contentType().toString()
+                    )
+                    context.contentResolver.insert(
+                        MediaStore.Images.Media.EXTERNAL_CONTENT_URI,
+                        this
+                    )
+                    // 当相同路径下的文件，在文件管理器中被手动删除时，就会插入失败
+                } ?: throw NullPointerException("Uri insert failed. Try changing filename")
+                return fileUri
             } else {
                 // 低于 android 10， 使用 File 操作
-                val parentFile: File
-                if (item.illust.isR18File && Shaft.sSettings.isR18DivideSave) {
-                    parentFile = File(Settings.FILE_PATH_SINGLE_R18)
-                } else {
-                    parentFile = File(Settings.FILE_PATH_SINGLE)
-                }
+                val parentFile: File = File(getRelativePath())
                 if (!parentFile.exists()) {
                     parentFile.mkdir()
                 }
@@ -156,4 +98,26 @@ class Android10DownloadFactory22 constructor(
             }
         }
     }
+
+    private fun getFileName(): String {
+        return getRelativePath() + File.separator + item.name
+    }
+
+    private fun getFileNameQ(): String {
+        return getRelativePathQ() + File.separator + item.name
+    }
+
+    private fun getRelativePath(): String {
+        return PathUtils.getExternalPicturesPath() + File.separator + getShaftDir()
+    }
+
+    private fun getRelativePathQ(): String {
+        return Environment.DIRECTORY_PICTURES + File.separator + getShaftDir()
+    }
+
+    private fun getShaftDir(): String {
+        return if (isSaveToR18Dir()) "ShaftImages-R18" else "ShaftImages"
+    }
+
+    private fun isSaveToR18Dir() = item.illust.isR18File && Shaft.sSettings.isR18DivideSave
 }
