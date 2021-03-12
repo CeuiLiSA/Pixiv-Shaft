23c8118e24e2695eb81c77a53dee5207a7479767
CeuiLiSA <32817377+CeuiLiSA@users.noreply.github.com>
2021-03-12 11:33:22 +0800

    一点点重构

--- app/src/main/java/ceui/lisa/core/RxRun.java
+++ app/src/main/java/ceui/lisa/core/RxRun.java
@@ -1,44 +1,55 @@
 package ceui.lisa.core;
 
 
-import androidx.annotation.NonNull;
-
 import io.reactivex.Observable;
-import io.reactivex.ObservableEmitter;
-import io.reactivex.ObservableOnSubscribe;
 import io.reactivex.Observer;
 import io.reactivex.android.schedulers.AndroidSchedulers;
+import io.reactivex.functions.Function;
 import io.reactivex.schedulers.Schedulers;
 
 public class RxRun {
 
     /**
-     * runnable在新线程运行，observer在主线程回调，
+     * runnable在新线程运行，observer在主线程回调。
+     * 默认第一阶段发射的参数与第二阶段接收的参数类型不一致，使用rxjava-map做了一波转换。
+     * 其实也可以把Function做的转换操作放到subscribe里面执行
      *
      * @param runnable 需要异步执行的代码片段
      * @param observer 处理异步执行的结果
-     * @param <T> 类型参数
+     * @param mapper   类型转换
+     * @param <T>      emitter发射类型
+     * @param <R>      observer接收类型
      */
-    public static <T> void runOn(RxRunnable<T> runnable, Observer<T> observer) {
-        Observable<T> observable = Observable.create(new ObservableOnSubscribe<T>() {
-            @Override
-            public void subscribe(@NonNull ObservableEmitter<T> emitter) {
-                try {
-                    if (runnable != null) {
-                        T result = runnable.execute();
-                        if (result != null) {
-                            emitter.onNext(result);
-                        }
-                        emitter.onComplete();
+    public static <T, R> void runOn(RxRunnable<T> runnable, Observer<R> observer, Function<T, R> mapper) {
+        Observable<T> observable = Observable.create(emitter -> {
+            try {
+                if (runnable != null) {
+                    T result = runnable.execute();
+                    if (result != null) {
+                        emitter.onNext(result);
                     }
-                } catch (Exception e) {
-                    emitter.onError(e);
-                    e.printStackTrace();
+                    emitter.onComplete();
                 }
+            } catch (Exception e) {
+                emitter.onError(e);
+                e.printStackTrace();
             }
         });
         observable.subscribeOn(Schedulers.newThread())
                 .observeOn(AndroidSchedulers.mainThread())
+                .map(mapper)
                 .subscribe(observer);
+    }
+
+
+    /**
+     * 方法功能同上，只不过操作的参数类型一致，
+     *
+     * @param runnable 需要异步执行的代码片段
+     * @param observer 处理异步执行的结果
+     * @param <T>      类型参数
+     */
+    public static <T> void runOn(RxRunnable<T> runnable, Observer<T> observer) {
+        runOn(runnable, observer, t -> t);
     }
 }
