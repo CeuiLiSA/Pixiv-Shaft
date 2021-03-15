e62f5b5fb5f494d4b18c546f99e9ed1a9d81d423
sunbeams001 <sunbeams001@gmail.com>
2021-03-15 19:48:23 +0800

    更新 jsdelivr 地址为 master 分支最新地址，不依赖 tag

--- app/src/main/java/ceui/lisa/http/ResourceApi.java
+++ app/src/main/java/ceui/lisa/http/ResourceApi.java
@@ -6,9 +6,7 @@ import retrofit2.http.GET;
 
 public interface ResourceApi {
     String JSDELIVR_BASE_URL = "https://cdn.jsdelivr.net/";
-//    String JSDELIVR_BASE_URL = "https://raw.githubusercontent.com/";
 
-    @GET("gh/CeuiLiSA/Pixiv-Shaft/app/src/main/assets/comment.filter.rule.txt")
-//    @GET("CeuiLiSA/Pixiv-Shaft/master/app/src/main/assets/comment.filter.rule.txt")
+    @GET("gh/CeuiLiSA/Pixiv-Shaft@master/app/src/main/assets/comment.filter.rule.txt")
     Observable<ResponseBody> getCommentFilterRule();
 }
