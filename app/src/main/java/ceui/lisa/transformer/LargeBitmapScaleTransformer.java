e186d1f2944748e5fe1779105778ec05b956f592
sunbeams001 <sunbeams001@gmail.com>
2021-04-27 21:41:44 +0800

    插画详情 过大 Bitmap 绘制问题

--- app/src/main/java/ceui/lisa/transformer/LargeBitmapScaleTransformer.java
+++ app/src/main/java/ceui/lisa/transformer/LargeBitmapScaleTransformer.java
@@ -10,24 +10,24 @@ import java.security.MessageDigest;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
-import ceui.lisa.utils.OpenGLUtils;
 
 public class LargeBitmapScaleTransformer extends BitmapTransformation {
 
     private static final String ID = "ceui.lisa.transformer.LargeBitmapScaleTransformer";
     private static final byte[] ID_BYTES = ID.getBytes();
+    private static final int MAX_BITMAP_SIZE = 100 * 1024 * 1024;
 
     @Override
     protected Bitmap transform(@NonNull BitmapPool pool, @NonNull Bitmap toTransform, int outWidth, int outHeight) {
         int width = toTransform.getWidth();
         int height = toTransform.getHeight();
+        int byteCount = toTransform.getByteCount();
 
-        int renderLimit = OpenGLUtils.RENDER_LIMIT;
-        if(height > renderLimit){
-            width = width * renderLimit / height;
-            height = renderLimit;
-            Bitmap result = ImageUtils.compressByScale(toTransform, width, height);
-            return result;
+        if (byteCount > MAX_BITMAP_SIZE) {
+            double scale = Math.sqrt((double) byteCount / MAX_BITMAP_SIZE);
+            width = (int) Math.floor(width / scale);
+            height = (int) Math.floor(height / scale);
+            toTransform = ImageUtils.compressByScale(toTransform, width, height);
         }
 
         return toTransform;
@@ -44,7 +44,7 @@ public class LargeBitmapScaleTransformer extends BitmapTransformation {
     }
 
     @Override
-    public void updateDiskCacheKey(@NonNull MessageDigest messageDigest)  {
+    public void updateDiskCacheKey(@NonNull MessageDigest messageDigest) {
         messageDigest.update(ID_BYTES);
     }
 }
