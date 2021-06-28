7708d8888f50e93d595cb2d56940ae208ec627be
sunbeams001 <sunbeams001@gmail.com>
2021-06-28 09:07:51 +0800

    插画二级详情导航栏 flag 调整

--- app/src/main/java/ceui/lisa/activities/ImageDetailActivity.java
+++ app/src/main/java/ceui/lisa/activities/ImageDetailActivity.java
@@ -176,13 +176,21 @@ public class ImageDetailActivity extends BaseActivity<ActivityImageDetailBinding
         }
     }
 
-    private void refreshSystemUiVisibility(){
-        final int includeFlag = View.SYSTEM_UI_FLAG_FULLSCREEN | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION;
+    private void refreshSystemUiVisibility() {
+        final int includeFlag = View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
+                | View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                | View.SYSTEM_UI_FLAG_FULLSCREEN
+                | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                | View.SYSTEM_UI_FLAG_FULLSCREEN;
         int excludeFlag = 0;
-        if(Shaft.sSettings.isIllustDetailShowNavbar()){
-            excludeFlag = excludeFlag | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION;
+        if (Shaft.sSettings.isIllustDetailShowNavbar()) {
+            excludeFlag = excludeFlag
+                    | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                    | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION;
         }
         final int flag = getWindow().getDecorView().getSystemUiVisibility();
-        getWindow().getDecorView().setSystemUiVisibility(flag |includeFlag & ~excludeFlag);
+        getWindow().getDecorView().setSystemUiVisibility(flag | includeFlag & ~excludeFlag);
     }
 }
