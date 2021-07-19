5b419d64e15092962f3bd602b4dded8afb57e839
sunbeams001 <sunbeams001@gmail.com>
2021-07-19 20:43:46 +0800

    热度（有限）查询支持起止时间，移除一些bintray依赖（终止服务）

--- app/src/main/java/ceui/lisa/http/AppApi.java
+++ app/src/main/java/ceui/lisa/http/AppApi.java
@@ -300,9 +300,12 @@ public interface AppApi {
                                     @Query("word") String word);
 
 
-    @GET("v1/search/popular-preview/illust?filter=for_android&include_translated_tag_results=true&merge_plain_keyword_results=true&search_target=exact_match_for_tags")
+    @GET("v1/search/popular-preview/illust?filter=for_android&include_translated_tag_results=true&merge_plain_keyword_results=true")
     Observable<ListIllust> popularPreview(@Header("Authorization") String token,
-                                          @Query("word") String word);
+                                          @Query("word") String word,
+                                          @Query("start_date") String startDate,
+                                          @Query("end_date") String endDate,
+                                          @Query("search_target") String search_target);
 
     @GET("v1/search/popular-preview/novel?filter=for_android&include_translated_tag_results=true&merge_plain_keyword_results=true&search_target=exact_match_for_tags")
     Observable<ListNovel> popularNovelPreview(@Header("Authorization") String token,
