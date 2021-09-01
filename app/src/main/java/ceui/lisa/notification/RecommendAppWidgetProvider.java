e0859cec837abfb5c1f7c5f06273a7a58f12b807
sunbeams001 <sunbeams001@gmail.com>
2021-09-01 17:13:20 +0800

    App Widget 加圆角

--- app/src/main/java/ceui/lisa/notification/RecommendAppWidgetProvider.java
+++ app/src/main/java/ceui/lisa/notification/RecommendAppWidgetProvider.java
@@ -11,6 +11,8 @@ import android.os.IBinder;
 import android.widget.RemoteViews;
 
 import com.bumptech.glide.Glide;
+import com.bumptech.glide.load.resource.bitmap.RoundedCorners;
+import com.bumptech.glide.request.RequestOptions;
 import com.bumptech.glide.request.target.AppWidgetTarget;
 
 import java.io.Serializable;
@@ -137,6 +139,7 @@ public class RecommendAppWidgetProvider extends AppWidgetProvider {
                                 Glide.with(RecommendAppWidgetService.this)
                                         .asBitmap()
                                         .load(GlideUtil.getSquare(randomIllust))
+                                        .apply(new RequestOptions().transform(new RoundedCorners(20)))
                                         .into(target);
 
                                 manager.updateAppWidget(appID, views);
