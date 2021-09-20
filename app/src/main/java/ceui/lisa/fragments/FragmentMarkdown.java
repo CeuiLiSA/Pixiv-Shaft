32d724d86ccc520653228e1dec22dfcb194212bb
sunbeams001 <sunbeams001@gmail.com>
2021-09-20 21:31:06 +0800

    markdown 解析相对链接

--- app/src/main/java/ceui/lisa/fragments/FragmentMarkdown.java
+++ app/src/main/java/ceui/lisa/fragments/FragmentMarkdown.java
@@ -2,15 +2,21 @@ package ceui.lisa.fragments;
 
 import android.os.Bundle;
 import android.text.TextUtils;
+import android.view.View;
 
 import java.io.IOException;
 
+import androidx.annotation.NonNull;
 import ceui.lisa.R;
 import ceui.lisa.databinding.FragmentMarkdownBinding;
 import ceui.lisa.http.NullCtrl;
+import ceui.lisa.http.ResourceApi;
 import ceui.lisa.http.Retro;
 import ceui.lisa.utils.Params;
+import io.noties.markwon.AbstractMarkwonPlugin;
+import io.noties.markwon.LinkResolverDef;
 import io.noties.markwon.Markwon;
+import io.noties.markwon.MarkwonConfiguration;
 import io.reactivex.android.schedulers.AndroidSchedulers;
 import io.reactivex.schedulers.Schedulers;
 import okhttp3.ResponseBody;
@@ -53,7 +59,23 @@ public class FragmentMarkdown extends BaseFragment<FragmentMarkdownBinding> {
                                 return;
                             }
 
-                            final Markwon markwon = Markwon.create(mContext);
+                            final Markwon markwon = Markwon.builder(mContext)
+                                    .usePlugin(new AbstractMarkwonPlugin() {
+                                        @Override
+                                        public void configureConfiguration(@NonNull MarkwonConfiguration.Builder builder) {
+                                            builder.linkResolver(new LinkResolverDef() {
+                                                @Override
+                                                public void resolve(@NonNull View view, @NonNull String link) {
+                                                    if (link.startsWith("./")) {
+                                                        String newLink = ResourceApi.JSDELIVR_BASE_URL + ResourceApi.JSDELIVR_PROJECT_MASTER_PATH + link;
+                                                        super.resolve(view, newLink);
+                                                    } else {
+                                                        super.resolve(view, link);
+                                                    }
+                                                }
+                                            });
+                                        }
+                                    }).build();
                             markwon.setMarkdown(baseBind.text, content);
 
                         } catch (IOException e) {
--- app/src/main/java/ceui/lisa/http/ResourceApi.java
+++ app/src/main/java/ceui/lisa/http/ResourceApi.java
@@ -7,10 +7,11 @@ import retrofit2.http.Path;
 
 public interface ResourceApi {
     String JSDELIVR_BASE_URL = "https://cdn.jsdelivr.net/";
+    String JSDELIVR_PROJECT_MASTER_PATH = "gh/CeuiLiSA/Pixiv-Shaft@master/";
 
     @GET("gh/CeuiLiSA/Pixiv-Shaft@master/app/src/main/assets/comment.filter.rule.txt")
     Observable<ResponseBody> getCommentFilterRule();
 
-    @GET("gh/CeuiLiSA/Pixiv-Shaft@master/{path}")
+    @GET(JSDELIVR_PROJECT_MASTER_PATH + "{path}")
     Observable<ResponseBody> getByPath(@Path("path") String path);
 }
