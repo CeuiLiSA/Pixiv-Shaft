e90a4d032df94b91a5afa38402afc00ab0b93aec
sunbeams001 <sunbeams001@gmail.com>
2022-01-20 21:12:22 +0800

    ascii2d 404 修复

--- app/src/main/java/ceui/lisa/http/Ascii2DApi.java
+++ app/src/main/java/ceui/lisa/http/Ascii2DApi.java
@@ -4,6 +4,7 @@ import io.reactivex.Observable;
 import okhttp3.MultipartBody;
 import okhttp3.ResponseBody;
 import retrofit2.Response;
+import retrofit2.http.Headers;
 import retrofit2.http.Multipart;
 import retrofit2.http.POST;
 import retrofit2.http.Part;
@@ -11,5 +12,6 @@ import retrofit2.http.Part;
 public interface Ascii2DApi {
     @Multipart
     @POST("/search/file")
+    @Headers("User-Agent: RetrofitClient")
     Observable<Response<ResponseBody>> query(@Part MultipartBody.Part part);
 }
