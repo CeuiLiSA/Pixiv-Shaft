0dc7f49bd3f5a5d37bb246daae2c58109bb6e860
sunbeams001 <sunbeams001@gmail.com>
2022-04-01 22:53:30 +0800

    关注者的小说显示公开+私人

--- app/src/main/java/ceui/lisa/repo/NewNovelRepo.kt
+++ app/src/main/java/ceui/lisa/repo/NewNovelRepo.kt
@@ -3,12 +3,13 @@ package ceui.lisa.repo
 import ceui.lisa.core.RemoteRepo
 import ceui.lisa.http.Retro
 import ceui.lisa.model.ListNovel
+import ceui.lisa.utils.Params
 import io.reactivex.Observable
 
 class NewNovelRepo : RemoteRepo<ListNovel>() {
 
     override fun initApi(): Observable<ListNovel> {
-        return Retro.getAppApi().getBookedUserSubmitNovel(token())
+        return Retro.getAppApi().getBookedUserSubmitNovel(token(), Params.TYPE_ALL)
     }
 
     override fun initNextApi(): Observable<ListNovel> {
