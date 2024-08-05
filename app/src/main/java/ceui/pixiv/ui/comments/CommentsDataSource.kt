30fd4afc5d2cb2aabc379759223ef08688f46498
LoxiaLiSA <xin@projz.com>
2024-08-05 12:30:58 +0800

    comments

--- app/src/main/java/ceui/pixiv/ui/comments/CommentsDataSource.kt
+++ app/src/main/java/ceui/pixiv/ui/comments/CommentsDataSource.kt
@@ -1,12 +1,11 @@
 package ceui.pixiv.ui.comments
 
-import androidx.lifecycle.MutableLiveData
 import ceui.loxia.Client
 import ceui.loxia.Comment
 import ceui.loxia.CommentResponse
 import ceui.loxia.ProgressTextButton
-import ceui.loxia.RefreshState
 import ceui.pixiv.ui.common.DataSource
+import ceui.pixiv.ui.common.ListItemHolder
 
 class CommentsDataSource(
     private val args: CommentsFragmentArgs,
@@ -29,15 +28,34 @@ class CommentsDataSource(
             sender.showProgress()
             val resp = Client.appApi.getIllustReplyComments(commentId)
             childCommentsMap[commentId] = resp.comments
-            updateItem<CommentHolder>(commentId) { old ->
+            updateItem(commentId) { old ->
                 CommentHolder(
                     old.comment,
-                    args.illustArthurId,
+                    old.illustArthurId,
                     resp.comments,
                 )
             }
         } finally {
             sender.hideProgress()
+        }
+    }
+
+    private fun updateItem(id: Long, update: (CommentHolder) -> CommentHolder) {
+        val itemHolders = pickItemHolders()
+        itemHolders.value?.let { currentHolders ->
+            val index = currentHolders.indexOfFirst { it.getItemId() == id }
+            if (index != -1) {
+                try {
+                    val target = currentHolders[index] as CommentHolder
+                    val updated = update(target)
+                    val updatedHolders = currentHolders.toMutableList().apply {
+                        set(index, updated)
+                    }
+                    itemHolders.value = updatedHolders
+                } catch (ex: Exception) {
+                    ex.printStackTrace()
+                }
+            }
         }
     }
 }
