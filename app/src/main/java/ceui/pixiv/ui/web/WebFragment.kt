b6f3b053b5ad8478f552f2b8cf47b2fa3d0e57fc
LoxiaLiSA <xin@supersymmetrycorp.com>
2024-09-05 23:06:17 +0800

    offset page

--- app/src/main/java/ceui/pixiv/ui/web/WebFragment.kt
+++ app/src/main/java/ceui/pixiv/ui/web/WebFragment.kt
@@ -4,13 +4,21 @@ import android.os.Bundle
 import android.view.View
 import android.webkit.CookieManager
 import android.webkit.WebChromeClient
+import android.webkit.WebResourceRequest
+import android.webkit.WebResourceResponse
 import android.webkit.WebSettings
 import android.webkit.WebView
 import android.webkit.WebViewClient
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.core.view.updatePadding
 import ceui.lisa.R
 import ceui.lisa.databinding.FragmentWebBinding
 import ceui.lisa.utils.Common
+import ceui.pixiv.ui.common.HomeTabContainer
 import ceui.pixiv.ui.common.PixivFragment
+import ceui.pixiv.ui.common.setUpToolbar
+import com.google.gson.Gson
 import com.tencent.mmkv.MMKV
 
 class WebFragment : PixivFragment(R.layout.fragment_web) {
@@ -23,6 +31,11 @@ class WebFragment : PixivFragment(R.layout.fragment_web) {
         super.onViewCreated(view, savedInstanceState)
 
         val binding = FragmentWebBinding.bind(view)
+        ViewCompat.setOnApplyWindowInsetsListener(binding.root) { v, windowInsets ->
+            val insets = windowInsets.getInsets(WindowInsetsCompat.Type.systemBars())
+            binding.webView.updatePadding(0, insets.top, 0, insets.bottom)
+            WindowInsetsCompat.CONSUMED
+        }
 
         val webSettings: WebSettings = binding.webView.settings
 
@@ -35,11 +48,25 @@ class WebFragment : PixivFragment(R.layout.fragment_web) {
                     prefStore.putString("web-api-cookie", cookie)
                 }
             }
+
+            override fun shouldInterceptRequest(
+                view: WebView?,
+                request: WebResourceRequest?
+            ): WebResourceResponse? {
+                val requestUrlString = request?.url?.toString()
+                if (requestUrlString?.contains("ajax/user/bookmarks") == true) {
+                    val headersMap: Map<String, String> = request.requestHeaders
+                    val jsonHeaders: String = Gson().toJson(headersMap)
+                    prefStore.putString("web-api-header", jsonHeaders)
+                    Common.showLog("asewsd requestUrlString ${requestUrlString}")
+                    Common.showLog("asewsd jsonHeaders ${jsonHeaders}")
+                }
+                return super.shouldInterceptRequest(view, request)
+            }
         }
         binding.webView.webChromeClient = object : WebChromeClient() {
 
         }
-        webSettings.userAgentString = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/123.0.0.0 Safari/537.36"
         // UI
         webSettings.useWideViewPort = true //-> 缩放至屏幕大小
         webSettings.loadWithOverviewMode = true// -> 缩放至屏幕大小
