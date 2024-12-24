ce1184b8844fc4f6334fa273156a89cc8a2098f2
LoxiaLiSA <xin@projz.com>
2024-12-24 15:21:13 +0800

    aa

--- app/src/main/java/ceui/pixiv/ui/detail/ArtworkFragment.kt
+++ app/src/main/java/ceui/pixiv/ui/detail/ArtworkFragment.kt
@@ -2,10 +2,13 @@ package ceui.pixiv.ui.detail
 
 import android.os.Bundle
 import android.view.View
+import androidx.recyclerview.widget.LinearLayoutManager
 import ceui.pixiv.ui.common.PixivFragment
 import ceui.lisa.R
 import ceui.lisa.databinding.FragmentPixivListBinding
 import ceui.lisa.utils.GlideUrlChild
+import ceui.lisa.view.LinearItemDecoration
+import ceui.lisa.view.LinearItemDecorationKt
 import ceui.loxia.Illust
 import ceui.loxia.ObjectPool
 import ceui.loxia.threadSafeArgs
@@ -16,6 +19,7 @@ import ceui.pixiv.ui.common.pixivValueViewModel
 import ceui.pixiv.ui.common.setUpRefreshState
 import ceui.pixiv.ui.home.RecmdNovelDataSource
 import ceui.pixiv.ui.list.pixivListViewModel
+import ceui.refactor.ppppx
 import ceui.refactor.viewBinding
 import com.bumptech.glide.Glide
 import com.bumptech.glide.request.RequestOptions.bitmapTransform
@@ -32,12 +36,18 @@ class ArtworkFragment : PixivFragment(R.layout.fragment_pixiv_list), FitsSystemW
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        ObjectPool.get<Illust>(safeArgs.illustId).observe(viewLifecycleOwner) { illust ->
+        val liveIllust = ObjectPool.get<Illust>(safeArgs.illustId)
+        liveIllust.observe(viewLifecycleOwner) { illust ->
             Glide.with(this)
                 .load(GlideUrlChild(illust.image_urls?.large))
                 .apply(bitmapTransform(BlurTransformation(25, 3)))
                 .into(binding.pageBackground)
         }
-        setUpRefreshState(binding, viewModel, ListMode.VERTICAL_NO_MARGIN)
+        val ctx = requireContext()
+        binding.listView.layoutManager = LinearLayoutManager(ctx)
+        liveIllust.value?.let { illust ->
+            binding.listView.addItemDecoration(LinearItemDecorationKt(16.ppppx, illust.page_count))
+        }
+        setUpRefreshState(binding, viewModel, ListMode.CUSTOM)
     }
 }
