940048940d99755d153a5440f8c695937c0ef8f9
LoxiaLiSA <xin@projz.com>
2025-01-06 17:23:40 +0800

    aa

--- app/src/main/java/ceui/pixiv/ui/novel/NovelHeaderHolder.kt
+++ app/src/main/java/ceui/pixiv/ui/novel/NovelHeaderHolder.kt
@@ -1,14 +1,19 @@
 package ceui.pixiv.ui.novel
 
 import android.view.View
+import ceui.lisa.R
 import ceui.lisa.annotations.ItemHolder
 import ceui.lisa.databinding.CellNovelHeaderBinding
 import ceui.loxia.Novel
 import ceui.loxia.ObjectPool
 import ceui.loxia.Series
 import ceui.loxia.findActionReceiverOrNull
+import ceui.loxia.findFragmentOrNull
+import ceui.loxia.pushFragment
+import ceui.pixiv.ui.common.ImgUrlFragmentArgs
 import ceui.pixiv.ui.common.ListItemHolder
 import ceui.pixiv.ui.common.ListItemViewHolder
+import ceui.pixiv.ui.common.PixivFragment
 import ceui.pixiv.utils.setOnClick
 
 
@@ -25,6 +30,17 @@ class NovelHeaderViewHolder(bd: CellNovelHeaderBinding) : ListItemViewHolder<Cel
         super.onBindViewHolder(holder, position)
         val liveNovel = ObjectPool.get<Novel>(holder.novelId)
         binding.novel = liveNovel
+        binding.novelCover.setOnClick { sender ->
+            liveNovel.value?.image_urls?.findMaxSizeUrl()?.let { url ->
+                sender.findFragmentOrNull<PixivFragment>()?.pushFragment(
+                    R.id.navigation_img_url,
+                    ImgUrlFragmentArgs(
+                        url,
+                        "novel_${holder.novelId}_cover.png"
+                    ).toBundle()
+                )
+            }
+        }
         binding.seriesName.setOnClick { sender ->
             liveNovel.value?.series?.let { series ->
                 sender.findActionReceiverOrNull<NovelSeriesActionReceiver>()?.onClickSeries(sender, series)
