9e620f538e91cd5eb3950e02efb569ea83bcbd3b
LoxiaLiSA <xin@projz.com>
2025-04-15 16:18:45 +0800

    aa

--- app/src/main/java/ceui/pixiv/ui/discover/DiscoverAllViewModel.kt
+++ app/src/main/java/ceui/pixiv/ui/discover/DiscoverAllViewModel.kt
@@ -2,19 +2,26 @@ package ceui.pixiv.ui.discover
 
 import ceui.loxia.Client
 import ceui.loxia.HomeData
+import ceui.loxia.HomeOneLine
+import ceui.loxia.Illust
 import ceui.loxia.MainBody
 import ceui.loxia.NextPageSpec
+import ceui.loxia.Novel
+import ceui.loxia.ObjectType
 import ceui.loxia.RefreshHint
 import ceui.loxia.RefreshState
 import ceui.pixiv.ui.common.HoldersViewModel
+import ceui.pixiv.ui.common.ListItemHolder
+import ceui.pixiv.ui.common.NovelCardHolder
 import ceui.pixiv.ui.common.createResponseStore
+import ceui.pixiv.ui.user.UserPostHolder
+import com.google.gson.Gson
 
 class DiscoverAllViewModel : HoldersViewModel() {
 
     private var _nextPageSpec: NextPageSpec? = null
-
+    private val gson = Gson()
     private val responseStore = createResponseStore<HomeData>({ "home-all" })
-
 
     override suspend fun refreshImpl(hint: RefreshHint) {
         super.refreshImpl(hint)
@@ -23,7 +30,7 @@ class DiscoverAllViewModel : HoldersViewModel() {
             responseStore.writeToCache(it)
         }
         _nextPageSpec = resp.next_params
-        val records = resp.displayList.map { SpecHolder(it) }
+        val records = mapHolders(resp.displayList)
         _itemHolders.value = records
         _refreshState.value = RefreshState.LOADED(
             hasContent = records.isNotEmpty(),
@@ -36,13 +43,27 @@ class DiscoverAllViewModel : HoldersViewModel() {
         val nextPageSpec = _nextPageSpec ?: return
         val resp = Client.appApi.getHomeAll(MainBody(next_params = nextPageSpec))
         _nextPageSpec = resp.next_params
-        val records = resp.displayList.map { SpecHolder(it) }
+        val records = mapHolders(resp.displayList)
         _itemHolders.value = ((_itemHolders.value ?: listOf()) + records)
         _refreshState.value = RefreshState.LOADED(
             hasContent = true,
             hasNext = _nextPageSpec != null
         )
     }
+
+    private fun mapHolders(items: List<HomeOneLine>): List<ListItemHolder> {
+        return items.mapNotNull { spec ->
+            val appModel = spec.thumbnails?.firstOrNull()?.app_model
+            val json = appModel?.let { gson.toJson(it) }.takeIf { !it.isNullOrEmpty() } ?: return@mapNotNull null
+
+            when (spec.kind) {
+                ObjectType.NOVEL -> gson.fromJson(json, Novel::class.java)?.let { NovelCardHolder(it) }
+                ObjectType.ILLUST, ObjectType.MANGA -> gson.fromJson(json, Illust::class.java)?.let { UserPostHolder(it) }
+                else -> null
+            }
+        }
+    }
+
 
     init {
         refresh(RefreshHint.InitialLoad)
