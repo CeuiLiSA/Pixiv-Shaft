07bf04f59a27b360fd882929d5ec16955eecae39
LoxiaLiSA <xin@projz.com>
2025-05-08 19:00:16 +0800

    aa

--- app/src/main/java/ceui/pixiv/ui/user/following/FollowingPostsDataSource.kt
+++ app/src/main/java/ceui/pixiv/ui/user/following/FollowingPostsDataSource.kt
@@ -4,21 +4,32 @@ import ceui.lisa.utils.Params
 import ceui.loxia.Client
 import ceui.loxia.Illust
 import ceui.loxia.IllustResponse
-import ceui.loxia.TrendingTag
-import ceui.loxia.TrendingTagsResponse
-import ceui.loxia.UserPreview
-import ceui.loxia.UserPreviewResponse
+import ceui.pixiv.session.SessionManager
 import ceui.pixiv.ui.common.DataSource
-import ceui.pixiv.ui.common.IllustCardHolder
-import ceui.pixiv.ui.common.ResponseStore
 import ceui.pixiv.ui.common.createResponseStore
 import ceui.pixiv.ui.user.UserPostHolder
-import ceui.pixiv.ui.user.UserPreviewHolder
+import com.tencent.mmkv.MMKV
 
 class FollowingPostsDataSource(
     private val args: FollowingPostFragmentArgs,
 ) : DataSource<Illust, IllustResponse>(
-    dataFetcher = { Client.appApi.followUserPosts(args.objectType, args.restrictType ?: Params.TYPE_ALL) },
+    dataFetcher = {
+        val prefStore = MMKV.mmkvWithID("api-cache-${SessionManager.loggedInUid}")
+        val resp =
+            Client.appApi.followUserPosts(args.objectType, args.restrictType ?: Params.TYPE_ALL)
+
+        val key = "latest-${args.objectType}-id"
+
+        val lastLatestIllustId = prefStore.getLong(key, 0L)
+        val lastIndex = resp.displayList.indexOfFirst { it.id == lastLatestIllustId }
+        if (lastIndex > 0) {
+        }
+
+        resp.displayList.getOrNull(0)?.let { latestIllust ->
+            prefStore.putLong(key, latestIllust.id)
+        }
+        resp
+    },
     responseStore = createResponseStore({ "following-user-${args.objectType}-api-${args.restrictType}" }),
     itemMapper = { illust -> listOf(UserPostHolder(illust)) },
     filter = { illust -> illust.isAuthurExist() }
