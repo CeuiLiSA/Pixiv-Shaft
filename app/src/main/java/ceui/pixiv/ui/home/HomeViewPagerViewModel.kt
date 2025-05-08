206edd9eadfc272ee8be94a061ba8807bc061305
LoxiaLiSA <xin@projz.com>
2025-05-08 18:53:15 +0800

    aa

--- app/src/main/java/ceui/pixiv/ui/home/HomeViewPagerViewModel.kt
+++ app/src/main/java/ceui/pixiv/ui/home/HomeViewPagerViewModel.kt
@@ -7,4 +7,18 @@ class HomeViewPagerViewModel : ViewModel() {
 
     val selectedTabIndex = MutableLiveData(0)
 
+    val composeButtonState = MutableLiveData(ComposeButtonState.CLOSED)
+
+    fun toggleComposeButton() {
+        if (composeButtonState.value == ComposeButtonState.CLOSED) {
+            composeButtonState.value = ComposeButtonState.OPEN
+        } else {
+            composeButtonState.value = ComposeButtonState.CLOSED
+        }
+    }
+
+    object ComposeButtonState {
+        const val CLOSED = 0
+        const val OPEN = 1
+    }
 }
