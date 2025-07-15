0d92e6506e43f3ef397dc63af029148612623095
LoxiaLiSA <xin@projz.com>
2025-07-15 11:33:49 +0800

    aa

--- app/src/main/java/ceui/pixiv/ui/task/TaskPool.kt
+++ app/src/main/java/ceui/pixiv/ui/task/TaskPool.kt
@@ -1,34 +1,46 @@
 package ceui.pixiv.ui.task
 
-import androidx.fragment.app.FragmentActivity
 import kotlinx.coroutines.CoroutineScope
 
 object TaskPool {
 
     private const val MAX_CACHE_SIZE = 256
 
-    // 使用 LinkedHashMap 来实现 LRU 缓存
-    private val _taskMap: LinkedHashMap<String, LoadTask> =
-        object : LinkedHashMap<String, LoadTask>(
-            MAX_CACHE_SIZE,
-            0.75f,
-            true
-        ) {
-            override fun removeEldestEntry(eldest: Map.Entry<String, LoadTask>?): Boolean {
-                return size > MAX_CACHE_SIZE
-            }
+    // 单独的 LoadTask 缓存
+    private val loadTaskMap = object : LinkedHashMap<String, LoadTask>(
+        MAX_CACHE_SIZE, 0.75f, true
+    ) {
+        override fun removeEldestEntry(eldest: Map.Entry<String, LoadTask>): Boolean {
+            return size > MAX_CACHE_SIZE
         }
+    }
 
-    fun getLoadTask(namedUrl: NamedUrl, coroutineScope: CoroutineScope, autoStart: Boolean = true): LoadTask {
-        return _taskMap.getOrPut(namedUrl.url) {
-            LoadTask(namedUrl, coroutineScope, autoStart)
+    // 单独的 DownloadTask 缓存
+    private val downloadTaskMap = object : LinkedHashMap<String, DownloadTask>(
+        MAX_CACHE_SIZE, 0.75f, true
+    ) {
+        override fun removeEldestEntry(eldest: Map.Entry<String, DownloadTask>): Boolean {
+            return size > MAX_CACHE_SIZE
+        }
+    }
+
+    fun getLoadTask(
+        namedUrl: NamedUrl,
+        coroutineScope: CoroutineScope,
+        autoStart: Boolean = true
+    ): LoadTask {
+        return synchronized(loadTaskMap) {
+            loadTaskMap.getOrPut(namedUrl.url) {
+                LoadTask(namedUrl, coroutineScope, autoStart)
+            }
         }
     }
 
     fun getDownloadTask(namedUrl: NamedUrl, coroutineScope: CoroutineScope): DownloadTask {
-        return (_taskMap[namedUrl.url] as? DownloadTask)
-            ?: DownloadTask(namedUrl, coroutineScope).also {
-                _taskMap[namedUrl.url] = it
+        return synchronized(downloadTaskMap) {
+            downloadTaskMap.getOrPut(namedUrl.url) {
+                DownloadTask(namedUrl, coroutineScope)
             }
+        }
     }
 }
