e014a9538e9c278c14a1bfb63096fb6e4b4a8286
LoxiaLiSA <xin@projz.com>
2025-07-16 17:04:49 +0800

    aa

--- app/src/main/java/ceui/pixiv/paging/OnePxHolder.kt
+++ app/src/main/java/ceui/pixiv/paging/OnePxHolder.kt
@@ -5,7 +5,15 @@ import ceui.lisa.databinding.ItemOnePxBinding
 import ceui.pixiv.ui.common.ListItemHolder
 import ceui.pixiv.ui.common.ListItemViewHolder
 
-class OnePxHolder() : ListItemHolder()
+class OnePxHolder() : ListItemHolder() {
+    override fun areItemsTheSame(other: ListItemHolder): Boolean {
+        return other is OnePxHolder
+    }
+
+    override fun areContentsTheSame(other: ListItemHolder): Boolean {
+        return other is OnePxHolder
+    }
+}
 
 @ItemHolder(OnePxHolder::class)
 class OnePxViewHolder(bd: ItemOnePxBinding) :
