f33fc9f6caabb984310153007e6dd7bf6c6b6f1c
LoxiaLiSA <xin@projz.com>
2025-07-18 11:01:53 +0800

    aa

--- app/src/main/java/ceui/pixiv/paging/PagingAPIRepository.kt
+++ app/src/main/java/ceui/pixiv/paging/PagingAPIRepository.kt
@@ -1,23 +1,27 @@
 package ceui.pixiv.paging
 
 import ceui.loxia.Client
-import ceui.loxia.IllustResponse
 import ceui.loxia.KListShow
 import ceui.pixiv.db.GeneralEntity
 import ceui.pixiv.ui.common.ListItemHolder
 import com.google.gson.Gson
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.withContext
+import java.lang.reflect.Type
 
 abstract class PagingAPIRepository<ObjectT> {
 
     private val gson = Gson()
 
+    private var responseType: Type? = null  // 用于缓存第一次解析类型
+
     abstract val recordType: Int
 
     suspend fun load(nextUrl: String?): KListShow<ObjectT> {
         if (nextUrl == null) {
-            return loadFirst()
+            return loadFirst().also {
+                responseType = getResponseTypeFromInstance(it)
+            }
         }
 
         return loadNext(nextUrl)
@@ -29,8 +33,15 @@ abstract class PagingAPIRepository<ObjectT> {
         return withContext(Dispatchers.IO) {
             val responseBody = Client.appApi.generalGet(nextUrl)
             val responseJson = responseBody.string()
-            gson.fromJson(responseJson, IllustResponse::class.java) as KListShow<ObjectT>
+            val type = responseType
+                ?: error("Response type not initialized. Must call load(null) first.")
+
+            gson.fromJson(responseJson, type)
         }
+    }
+
+    private fun getResponseTypeFromInstance(instance: KListShow<ObjectT>): Type {
+        return instance::class.java
     }
 
     abstract fun mapper(entity: GeneralEntity): List<ListItemHolder>
