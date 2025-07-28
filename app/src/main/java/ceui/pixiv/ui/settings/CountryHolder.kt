497eb78b56e466d580a68502298eb9e0dcd34374
SoxiaLiSA <ceuilisa@outlook.com>
2025-07-28 15:02:45 +0800

    gradle version

--- app/src/main/java/ceui/pixiv/ui/settings/CountryHolder.kt
+++ app/src/main/java/ceui/pixiv/ui/settings/CountryHolder.kt
@@ -7,20 +7,21 @@ import ceui.pixiv.ui.common.ListItemHolder
 import ceui.pixiv.ui.common.ListItemViewHolder
 import ceui.pixiv.utils.setOnClick
 
-class CountryHolder(val country: Country) : ListItemHolder() {
-}
+class CountryHolder(val country: Country) : ListItemHolder()
 
 @ItemHolder(CountryHolder::class)
-class CountryViewHolder(bd: CellCountryBinding) : ListItemViewHolder<CellCountryBinding, CountryHolder>(bd) {
+class CountryViewHolder(bd: CellCountryBinding) :
+    ListItemViewHolder<CellCountryBinding, CountryHolder>(bd) {
 
     override fun onBindViewHolder(holder: CountryHolder, position: Int) {
         super.onBindViewHolder(holder, position)
         binding.countryCodeInfo.text =
-            holder.country.name + " (${holder.country.nameCode.toUpperCase()})"
+            holder.country.name + " (${holder.country.nameCode.uppercase()})"
         binding.countryCodeNumber.text = "+" + holder.country.phoneCode
         binding.countryFlag.text = holder.country.flag
         binding.root.setOnClick {
-            it.findActionReceiverOrNull<SelectCountryActionReceiver>()?.selectCountry(holder.country)
+            it.findActionReceiverOrNull<SelectCountryActionReceiver>()
+                ?.selectCountry(holder.country)
         }
     }
 }
