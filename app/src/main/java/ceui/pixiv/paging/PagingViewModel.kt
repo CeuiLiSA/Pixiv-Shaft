90909fd6eb0c4ca6f9d8c9292ee72ba94954301f
SoxiaLiSA <ceuilisa@outlook.com>
2025-07-30 11:44:17 +0800

    aa

--- app/src/main/java/ceui/pixiv/paging/PagingViewModel.kt
+++ app/src/main/java/ceui/pixiv/paging/PagingViewModel.kt
@@ -9,19 +9,23 @@ import androidx.lifecycle.viewModelScope
 import androidx.paging.ExperimentalPagingApi
 import androidx.paging.Pager
 import androidx.paging.PagingConfig
+import androidx.paging.PagingData
 import androidx.paging.cachedIn
 import androidx.paging.flatMap
 import ceui.lisa.database.AppDatabase
 import ceui.lisa.models.ModelObject
 import ceui.loxia.requireNetworkStateManager
 import ceui.pixiv.db.GeneralEntity
+import ceui.pixiv.ui.common.ListItemHolder
 import ceui.pixiv.utils.NetworkStateManager
 import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.combine
 import kotlinx.coroutines.flow.flatMapLatest
 import kotlinx.coroutines.flow.map
 
+@OptIn(ExperimentalPagingApi::class, ExperimentalCoroutinesApi::class)
 class PagingViewModel<ObjectT : ModelObject>(
     private val db: AppDatabase,
     private val networkStateManager: NetworkStateManager,
@@ -31,78 +35,60 @@ class PagingViewModel<ObjectT : ModelObject>(
     private val refreshTrigger = MutableStateFlow(0)
     private val canAccessGoogle = networkStateManager.canAccessGoogle.asFlow()
 
-    @OptIn(ExperimentalPagingApi::class, ExperimentalCoroutinesApi::class)
-    val pager = when (repository) {
-        is PagingMediatorRepository -> {
-            combine(refreshTrigger, canAccessGoogle) { _, canAccess ->
-                canAccess
-            }.flatMapLatest { canAccess ->
-                if (!canAccess) {
-                    Pager(
-                        config = PagingConfig(
-                            pageSize = 30,
-                            initialLoadSize = 30,
-                            prefetchDistance = 0
-                        ),
-                        pagingSourceFactory = { NoOpPagingSource<GeneralEntity>() }
-                    ).flow.map { pagingData ->
-                        pagingData.flatMap(repository::mapper)
-                    }
-                } else {
-                    Pager(
-                        config = PagingConfig(
-                            pageSize = 30,
-                            initialLoadSize = 30,
-                            prefetchDistance = 0
-                        ),
-                        remoteMediator = PagingRemoteMediator(
-                            db,
-                            repository,
-                            repository.recordType
-                        ),
-                        pagingSourceFactory = {
-                            db.generalDao().pagingSource(repository.recordType)
-                        }
-                    ).flow.map { pagingData ->
-                        pagingData.flatMap(repository::mapper)
-                    }
-                }
-            }.cachedIn(viewModelScope)
-        }
+    val pager: Flow<PagingData<ListItemHolder>> =
+        combine(refreshTrigger, canAccessGoogle) { _, canAccess ->
+            canAccess
+        }.flatMapLatest { canAccess ->
+            when (repository) {
+                is PagingMediatorRepository -> createMediatorPager(repository, canAccess)
+                is PagingAPIRepository -> createApiPager(repository, canAccess)
+                else -> throw IllegalArgumentException("Unsupported repository type")
+            }
+        }.cachedIn(viewModelScope)
 
-        is PagingAPIRepository -> {
-            combine(refreshTrigger, canAccessGoogle) { _, canAccess ->
-                canAccess
-            }.flatMapLatest { canAccess ->
-                if (!canAccess) {
-                    Pager(
-                        config = PagingConfig(
-                            pageSize = 30,
-                            initialLoadSize = 30,
-                            prefetchDistance = 0
-                        ),
-                        pagingSourceFactory = { NoOpPagingSource<ObjectT>() }
-                    ).flow.map { pagingData ->
-                        pagingData.flatMap(repository::mapper)
-                    }
-                } else {
-                    Pager(
-                        config = PagingConfig(
-                            pageSize = 30,
-                            initialLoadSize = 30,
-                            prefetchDistance = 0
-                        ),
-                        pagingSourceFactory = { PagingAPISource(repository) }
-                    ).flow.map { pagingData ->
-                        pagingData.flatMap(repository::mapper)
-                    }
+    private fun createMediatorPager(
+        repository: PagingMediatorRepository<ObjectT>,
+        canAccess: Boolean
+    ): Flow<PagingData<ListItemHolder>> {
+        return if (!canAccess) {
+            Pager(
+                config = defaultPagingConfig(),
+                pagingSourceFactory = { NoOpPagingSource<GeneralEntity>() }
+            ).flow.map { it.flatMap(repository::mapper) }
+        } else {
+            Pager(
+                config = defaultPagingConfig(),
+                remoteMediator = PagingRemoteMediator(db, repository, repository.recordType),
+                pagingSourceFactory = {
+                    db.generalDao().pagingSource(repository.recordType)
                 }
-            }.cachedIn(viewModelScope)
+            ).flow.map { it.flatMap(repository::mapper) }
         }
+    }
 
-        else -> {
-            throw RuntimeException("Unsupported repository type")
+    private fun createApiPager(
+        repository: PagingAPIRepository<ObjectT>,
+        canAccess: Boolean
+    ): Flow<PagingData<ListItemHolder>> {
+        return if (!canAccess) {
+            Pager(
+                config = defaultPagingConfig(),
+                pagingSourceFactory = { NoOpPagingSource<ObjectT>() }
+            ).flow.map { it.flatMap(repository::mapper) }
+        } else {
+            Pager(
+                config = defaultPagingConfig(),
+                pagingSourceFactory = { PagingAPISource(repository) }
+            ).flow.map { it.flatMap(repository::mapper) }
         }
+    }
+
+    private fun defaultPagingConfig(): PagingConfig {
+        return PagingConfig(
+            pageSize = 30,
+            initialLoadSize = 30,
+            prefetchDistance = 0
+        )
     }
 
     fun refresh() {
@@ -111,6 +97,7 @@ class PagingViewModel<ObjectT : ModelObject>(
 
     val recordType: Int? get() = (repository as? PagingMediatorRepository)?.recordType
 }
+
 
 inline fun <ObjectT : ModelObject> Fragment.pagingViewModel(
     noinline repositoryProducer: () -> PagingMediatorRepository<ObjectT>,
