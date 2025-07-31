0e907d2bed23473d3c883a92d42c94a6d00eb84c
SoxiaLiSA <ceuilisa@outlook.com>
2025-07-31 15:17:38 +0800

    aa

--- app/src/main/java/ceui/pixiv/ui/search/SearchIlllustMangaFragment.kt
+++ app/src/main/java/ceui/pixiv/ui/search/SearchIlllustMangaFragment.kt
@@ -1,17 +1,22 @@
 package ceui.pixiv.ui.search
 
 import android.os.Bundle
+import android.view.LayoutInflater
 import android.view.View
+import androidx.core.view.isVisible
 import androidx.fragment.app.activityViewModels
 import androidx.fragment.app.viewModels
 import ceui.lisa.R
 import ceui.lisa.databinding.FragmentPagedListBinding
+import ceui.lisa.databinding.ItemRadioButtonsBinding
 import ceui.loxia.ObjectType
 import ceui.loxia.observeEvent
 import ceui.pixiv.paging.pagingViewModel
+import ceui.pixiv.ui.bottom.UsersYoriDialogFragment
 import ceui.pixiv.ui.common.PixivFragment
 import ceui.pixiv.ui.common.setUpPagedList
 import ceui.pixiv.ui.common.viewBinding
+import ceui.pixiv.utils.setOnClick
 import ceui.pixiv.widgets.DialogViewModel
 
 
@@ -39,31 +44,37 @@ class SearchIlllustMangaFragment : PixivFragment(R.layout.fragment_paged_list) {
             viewModel.refresh()
         }
 
-//        binding.radioTab.setTabs(
-//            listOf(
-//                "热度预览",
-//                "从新到旧",
-//                "从旧到新",
-//                "热度排序",
-//            )
-//        )
-//        binding.radioTab.setItemCickListener { index ->
-//            searchViewModel.illustSelectedRadioTabIndex.value = index
-//            val now = System.currentTimeMillis()
-//            searchViewModel.triggerSearchIllustMangaEvent(now)
-//        }
-//        searchViewModel.illustSelectedRadioTabIndex.observe(viewLifecycleOwner) { index ->
-//            binding.radioTab.selectTab(index)
-//            binding.usersYori.isVisible = (index == 1) || (index == 2)
-//        }
-//        dialogViewModel.chosenUsersYoriCount.observe(viewLifecycleOwner) { count ->
-//            binding.usersYori.text = "${count}users入り"
-//        }
-//        dialogViewModel.triggerUsersYoriEvent.observeEvent(this) { time ->
-//            searchViewModel.triggerSearchIllustMangaEvent(time)
-//        }
-//        binding.usersYori.setOnClick {
-//            UsersYoriDialogFragment().show(childFragmentManager, "UsersYoriDialogFragmentTag")
-//        }
+        val layout = ItemRadioButtonsBinding.inflate(LayoutInflater.from(requireContext()))
+
+        binding.listHeader.addView(layout.root)
+
+        val radioTab = layout.radioTab
+        val usersYori = layout.usersYori
+        radioTab.setTabs(
+            listOf(
+                "热度预览",
+                "从新到旧",
+                "从旧到新",
+                "热度排序",
+            )
+        )
+        radioTab.setItemCickListener { index ->
+            searchViewModel.illustSelectedRadioTabIndex.value = index
+            val now = System.currentTimeMillis()
+            searchViewModel.triggerSearchIllustMangaEvent(now)
+        }
+        searchViewModel.illustSelectedRadioTabIndex.observe(viewLifecycleOwner) { index ->
+            radioTab.selectTab(index)
+            usersYori.isVisible = (index == 1) || (index == 2)
+        }
+        dialogViewModel.chosenUsersYoriCount.observe(viewLifecycleOwner) { count ->
+            usersYori.text = "${count}users入り"
+        }
+        dialogViewModel.triggerUsersYoriEvent.observeEvent(this) { time ->
+            searchViewModel.triggerSearchIllustMangaEvent(time)
+        }
+        usersYori.setOnClick {
+            UsersYoriDialogFragment().show(childFragmentManager, "UsersYoriDialogFragmentTag")
+        }
     }
 }
--- app/src/main/java/ceui/pixiv/ui/search/SearchNovelFragment.kt
+++ app/src/main/java/ceui/pixiv/ui/search/SearchNovelFragment.kt
@@ -1,11 +1,14 @@
 package ceui.pixiv.ui.search
 
 import android.os.Bundle
+import android.view.LayoutInflater
 import android.view.View
+import androidx.core.view.isVisible
 import androidx.fragment.app.activityViewModels
 import androidx.fragment.app.viewModels
 import ceui.lisa.R
 import ceui.lisa.databinding.FragmentPagedListBinding
+import ceui.lisa.databinding.ItemRadioButtonsBinding
 import ceui.loxia.ObjectType
 import ceui.loxia.observeEvent
 import ceui.pixiv.paging.pagingViewModel
@@ -37,20 +40,30 @@ class SearchNovelFragment : PixivFragment(R.layout.fragment_paged_list) {
         searchViewModel.searchNovelEvent.observeEvent(viewLifecycleOwner) {
             viewModel.refresh()
         }
-//        binding.radioTab.setTabs(listOf(
-//            "热度预览",
-//            "从新到旧",
-//            "从旧到新",
-//            "热度排序",
-//        ))
-//        binding.radioTab.setItemCickListener { index ->
-//            searchViewModel.novelSelectedRadioTabIndex.value = index
-//            val now = System.currentTimeMillis()
-//            searchViewModel.triggerSearchNovelEvent(now)
-//        }
-//        searchViewModel.novelSelectedRadioTabIndex.observe(viewLifecycleOwner) { index ->
-//            binding.radioTab.selectTab(index)
-//            binding.usersYori.isVisible = (index == 1) || (index == 2)
-//        }
+
+        val layout = ItemRadioButtonsBinding.inflate(LayoutInflater.from(requireContext()))
+
+        binding.listHeader.addView(layout.root)
+
+        val radioTab = layout.radioTab
+        val usersYori = layout.usersYori
+
+        radioTab.setTabs(
+            listOf(
+                "热度预览",
+                "从新到旧",
+                "从旧到新",
+                "热度排序",
+            )
+        )
+        radioTab.setItemCickListener { index ->
+            searchViewModel.novelSelectedRadioTabIndex.value = index
+            val now = System.currentTimeMillis()
+            searchViewModel.triggerSearchNovelEvent(now)
+        }
+        searchViewModel.novelSelectedRadioTabIndex.observe(viewLifecycleOwner) { index ->
+            radioTab.selectTab(index)
+            usersYori.isVisible = (index == 1) || (index == 2)
+        }
     }
 }
