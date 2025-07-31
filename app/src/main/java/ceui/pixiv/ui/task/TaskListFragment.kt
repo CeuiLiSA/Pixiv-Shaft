933ffaed227201b9de5ec56a7f3e9c6ee244928b
SoxiaLiSA <ceuilisa@outlook.com>
2025-07-31 16:56:59 +0800

    aa

--- app/src/main/java/ceui/pixiv/ui/task/TaskListFragment.kt
+++ app/src/main/java/ceui/pixiv/ui/task/TaskListFragment.kt
@@ -23,33 +23,7 @@ class TaskListFragment : PixivFragment(R.layout.fragment_paged_list), TaskPrevie
 
     private val binding by viewBinding(FragmentPagedListBinding::bind)
     private val viewModel by pagingViewModel({ AppDatabase.getAppDatabase(requireContext()) }) { database ->
-        val maps = hashMapOf<String, List<Illust>>()
-        object : PagingAPIRepository<HumanReadableTask>() {
-            override suspend fun loadFirst(): KListShow<HumanReadableTask> {
-                val entities = database.generalDao().getAllByRecordType(RecordType.USER_TASK)
-                val humanReadableTasks = entities.map { entity ->
-                    entity.typedObject<HumanReadableTask>().also {
-                        maps[it.taskUUID] = loadIllustsFromCache(it.taskUUID) ?: listOf()
-                    }
-                }
-
-                return object : KListShow<HumanReadableTask> {
-                    override val displayList: List<HumanReadableTask>
-                        get() = humanReadableTasks
-                    override val nextPageUrl: String?
-                        get() = null
-                }
-            }
-
-            override fun mapper(entity: HumanReadableTask): List<ListItemHolder> {
-                return listOf(
-                    TaskPreviewHolder(
-                        entity,
-                        maps.getOrDefault(entity.taskUUID, listOf())
-                    )
-                )
-            }
-        }
+        Repository(database)
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
@@ -64,5 +38,36 @@ class TaskListFragment : PixivFragment(R.layout.fragment_paged_list), TaskPrevie
         pushFragment(
             R.id.navigation_cache_list, CacheFileFragmentArgs(task = humanReadableTask).toBundle()
         )
+    }
+
+    private class Repository(private val database: AppDatabase) :
+        PagingAPIRepository<HumanReadableTask>() {
+
+        private val maps = hashMapOf<String, List<Illust>>()
+
+        override suspend fun loadFirst(): KListShow<HumanReadableTask> {
+            val entities = database.generalDao().getAllByRecordType(RecordType.USER_TASK)
+            val humanReadableTasks = entities.map { entity ->
+                entity.typedObject<HumanReadableTask>().also {
+                    maps[it.taskUUID] = loadIllustsFromCache(it.taskUUID) ?: listOf()
+                }
+            }
+
+            return object : KListShow<HumanReadableTask> {
+                override val displayList: List<HumanReadableTask>
+                    get() = humanReadableTasks
+                override val nextPageUrl: String?
+                    get() = null
+            }
+        }
+
+        override fun mapper(entity: HumanReadableTask): List<ListItemHolder> {
+            return listOf(
+                TaskPreviewHolder(
+                    entity,
+                    maps.getOrDefault(entity.taskUUID, listOf())
+                )
+            )
+        }
     }
 }
