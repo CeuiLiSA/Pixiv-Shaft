1b90aa92fba8620b90a01c5fea61cf58314ad294
SoxiaLiSA <ceuilisa@outlook.com>
2025-09-15 18:39:54 +0800

    aa

--- app/src/main/java/ceui/pixiv/ui/user/UserBookmarkedIllustsFragment.kt
+++ app/src/main/java/ceui/pixiv/ui/user/UserBookmarkedIllustsFragment.kt
@@ -5,15 +5,8 @@ import android.view.View
 import androidx.navigation.fragment.navArgs
 import ceui.lisa.R
 import ceui.lisa.databinding.FragmentPagedListBinding
-import ceui.lisa.utils.Params
-import ceui.loxia.Client
-import ceui.loxia.ObjectType
 import ceui.pixiv.paging.pagingViewModel
-import ceui.pixiv.session.SessionManager
 import ceui.pixiv.ui.common.PixivFragment
-import ceui.pixiv.ui.common.TitledViewPagerFragment
-import ceui.pixiv.ui.common.pixivValueViewModel
-import ceui.pixiv.ui.common.repo.RemoteRepository
 import ceui.pixiv.ui.common.setUpPagedList
 import ceui.pixiv.ui.common.viewBinding
 
@@ -24,33 +17,9 @@ class UserBookmarkedIllustsFragment : PixivFragment(R.layout.fragment_paged_list
     private val viewModel by pagingViewModel({ safeArgs }) { args ->
         UserBookmarkedIllustsRepository(args)
     }
-    private val contentViewModel by pixivValueViewModel({ safeArgs }) { args ->
-        RemoteRepository {
-            val rest = if (args.restrictType == Params.TYPE_PRIVATE) {
-                "hide"
-            } else {
-                "show"
-            }
-            Client.webApi.getBookmarkedIllust(SessionManager.loggedInUid, ObjectType.ILLUST, rest)
-        }
-    }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        if (safeArgs.userId == SessionManager.loggedInUid) {
-            contentViewModel.result.observe(viewLifecycleOwner) { loadResult ->
-                (parentFragment as? TitledViewPagerFragment)?.let {
-                    val result = loadResult?.data ?: return@observe
-                    if (safeArgs.restrictType == Params.TYPE_PRIVATE) {
-                        it.getTitleLiveData(1).value =
-                            "${getString(R.string.string_392)} (${result.body?.total ?: 0})"
-                    } else {
-                        it.getTitleLiveData(0).value =
-                            "${getString(R.string.string_391)} (${result.body?.total ?: 0})"
-                    }
-                }
-            }
-        }
         setUpPagedList(binding, viewModel)
     }
 }
