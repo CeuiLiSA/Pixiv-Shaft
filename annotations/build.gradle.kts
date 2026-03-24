fe893ed15ac12f0741611ebd8249cde5879525e1
SoxiaLiSA <ceuilisa@outlook.com>
2026-03-24 12:16:43 +0800

    Fix search bar corner consistency

--- annotations/build.gradle.kts
+++ annotations/build.gradle.kts
@@ -4,6 +4,10 @@ plugins {
 }
 
 java {
-    sourceCompatibility = JavaVersion.VERSION_21
-    targetCompatibility = JavaVersion.VERSION_21
+    sourceCompatibility = JavaVersion.VERSION_17
+    targetCompatibility = JavaVersion.VERSION_17
+}
+
+kotlin {
+    jvmToolchain(17)
 }
--- processor/build.gradle.kts
+++ processor/build.gradle.kts
@@ -5,8 +5,12 @@ plugins {
 }
 
 java {
-    sourceCompatibility = JavaVersion.VERSION_21
-    targetCompatibility = JavaVersion.VERSION_21
+    sourceCompatibility = JavaVersion.VERSION_17
+    targetCompatibility = JavaVersion.VERSION_17
+}
+
+kotlin {
+    jvmToolchain(17)
 }
 
 dependencies {
